3dc1695243c2d4afece0f3d23f8b547d71f124a8
ASUS <khadiza70>
2022-01-24 10:31:29 +0600

    1st commit

---
+++ test.js
@@ -0,0 +1,36 @@
+// 
+
+
+
+// let a;
+//  a=7;
+
+// console.log(a);
+
+
+// 
+// let x = 5 ;
+// let y = 5 ;
+// let z = 6 ;
+// let myObjects ={x:5,y:5,z:6};
+
+// console.log(typeof myObject);
+
+// 
+// let x = 1;
+// let y = 1;
+
+
+// let m = (x && y) || !(y || (x&&y));
+
+// console.log(m);
+
+ function sleep(name,time){
+     console.log( name +" is sleeping " + time);
+ }
+ sleep("rahim",Date())
+ sleep("karim",Date())
+ sleep("mim",Date())
+ sleep("lima",Date())
+
+
